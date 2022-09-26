1f8782d07ad11f3ec8384b5bef77024a1be04607
Patrick Avery <patrick.avery@kitware.com>
2022-09-26 11:50:24 -0400

    Downgrade the LookingGlass SDK that Linux uses
    The latest Looking Glass SDK was built with too new of a glibc version
    for Linux. Downgrade it so that we can package it into wheels.
    
    We have unfortunately been unable to determine whether the older
    Looking Glass SDK will work with the new Looking Glass Bridge software.
    Hopefully, it does. If not, then we will have to require Linux users to
    use an older HoloPlay Service.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- FetchHoloPlayCore.cmake
+++ FetchHoloPlayCore.cmake
@@ -1,11 +1,21 @@
 include(FetchContent)
 
+if (UNIX AND NOT APPLE)
+  # Downgrade the version that Linux uses, because the latest version was
+  # built with too new of a glibc version.
+  set(lookingglass_file "HoloPlayCore-0.1.1-Open-20200923.tar.gz")
+  set(lookingglass_md5 b435316fa1f8454ba180e72608c3c28f)
+else ()
+  set(lookingglass_file "LookingGlassCoreSDK-Open-20220819.tgz")
+  set(lookingglass_md5 23a2a373c9d1c0f203251dc244f97f79)
+endif ()
+
 set(proj HoloPlayCore)
 set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
 FetchContent_Populate(${proj}
   SOURCE_DIR   ${EP_SOURCE_DIR}
-  URL          https://www.paraview.org/files/dependencies/LookingGlassCoreSDK-Open-20220819.tgz
-  URL_HASH MD5=23a2a373c9d1c0f203251dc244f97f79
+  URL          "https://www.paraview.org/files/dependencies/${lookingglass_file}"
+  URL_HASH     "MD5=${lookingglass_md5}"
   QUIET
   )
 
