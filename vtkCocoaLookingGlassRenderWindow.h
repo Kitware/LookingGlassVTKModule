Kitware/LookingGlassVTKModule

commit b99fa7324d798f98b1f00f2c6afa66fc38f7046f

Add several functions to the render window
This exposes several `vtkLookingGlassInterface` functions on the render
window classes, which make them easier to access, including in Python.
These are functions that we intend for users to have easy access to.

This also adds a `GetDeviceTypes()` function that can be called from
python (apparently, `vtkLookingGlassInterface::GetDevices()` does not
get a python interface, perhaps because of its return type).

Additionally, changing devices in `vtkLookingGlassInterface` is not
straightforward, and it's easier to just make a new one from scratch.
So, when the device type switches, re-create the interface.

Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

vtkCocoaLookingGlassRenderWindow.h
@@ -86,11 +86,90 @@ public:
    */
   std::string QuiltFileSuffix() const;
 
+  /**
+   * Turn on/off use of near and far clipping limits.
+   */
+  void SetUseClippingLimits(bool b);
+
+  /**
+   * Turn on/off use of near and far clipping limits.
+   */
+  bool GetUseClippingLimits() const;
+  vtkBooleanMacro(UseClippingLimits, bool);
+
+  /**
+   * Set/Get limit for the ratio of the far clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 1.2.
+   */
+  void SetFarClippingLimit(double d);
+
+  /**
+   * Set/Get limit for the ratio of the far clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 1.2.
+   */
+  double GetFarClippingLimit() const;
+
+  /**
+   * Set/Get limit for the ratio of the near clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 0.8.
+   */
+  void SetNearClippingLimit(double d);
+
+  /**
+   * Set/Get limit for the ratio of the near clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 0.8.
+   */
+  double GetNearClippingLimit() const;
+
+  /**
+   * Check if a movie quilt is currently being recorded.
+   */
+  bool IsRecordingQuilt() const;
+
+  /**
+   * Set/Get which LookingGlass device to use. DeviceIndex starts at 0 and
+   * increases.
+   */
+  void SetDeviceIndex(int i);
+
+  /**
+   * Set/Get which LookingGlass device to use. DeviceIndex starts at 0 and
+   * increases.
+   */
+  int GetDeviceIndex() const;
+
+  /**
+   * Set/Get which LookingGlass device type to target. This allows a quilt to be
+   * generated for a device that is not connected in the future.
+   */
+  void SetDeviceType(const std::string &t);
+
+  /**
+   * Set/Get which LookingGlass device type to target. This allows a quilt to be
+   * generated for a device that is not connected in the future.
+   */
+  std::string GetDeviceType() const;
+
+  /**
+   * Returns a vector of available device types.
+   */
+  static std::vector<std::string> GetDeviceTypes();
+
 protected:
   vtkCocoaLookingGlassRenderWindow();
   ~vtkCocoaLookingGlassRenderWindow() override;
 
-  vtkLookingGlassInterface* Interface;
+  vtkLookingGlassInterface* Interface = nullptr;
+
+  void InitializeInterface();
 
   void DoStereoRender() override;
   bool InStereoRender;

vtkWin32LookingGlassRenderWindow.h
@@ -81,11 +81,90 @@ public:
    */
   std::string QuiltFileSuffix() const;
 
+  /**
+   * Turn on/off use of near and far clipping limits.
+   */
+  void SetUseClippingLimits(bool b);
+
+  /**
+   * Turn on/off use of near and far clipping limits.
+   */
+  bool GetUseClippingLimits() const;
+  vtkBooleanMacro(UseClippingLimits, bool);
+
+  /**
+   * Set/Get limit for the ratio of the far clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 1.2.
+   */
+  void SetFarClippingLimit(double d);
+
+  /**
+   * Set/Get limit for the ratio of the far clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 1.2.
+   */
+  double GetFarClippingLimit() const;
+
+  /**
+   * Set/Get limit for the ratio of the near clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 0.8.
+   */
+  void SetNearClippingLimit(double d);
+
+  /**
+   * Set/Get limit for the ratio of the near clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 0.8.
+   */
+  double GetNearClippingLimit() const;
+
+  /**
+   * Check if a movie quilt is currently being recorded.
+   */
+  bool IsRecordingQuilt() const;
+
+  /**
+   * Set/Get which LookingGlass device to use. DeviceIndex starts at 0 and
+   * increases.
+   */
+  void SetDeviceIndex(int i);
+
+  /**
+   * Set/Get which LookingGlass device to use. DeviceIndex starts at 0 and
+   * increases.
+   */
+  int GetDeviceIndex() const;
+
+  /**
+   * Set/Get which LookingGlass device type to target. This allows a quilt to be
+   * generated for a device that is not connected in the future.
+   */
+  void SetDeviceType(const std::string &t);
+
+  /**
+   * Set/Get which LookingGlass device type to target. This allows a quilt to be
+   * generated for a device that is not connected in the future.
+   */
+  std::string GetDeviceType() const;
+
+  /**
+   * Returns a vector of available device types.
+   */
+  static std::vector<std::string> GetDeviceTypes();
+
 protected:
   vtkWin32LookingGlassRenderWindow();
   ~vtkWin32LookingGlassRenderWindow() override;
 
-  vtkLookingGlassInterface* Interface;
+  vtkLookingGlassInterface* Interface = nullptr;
+
+  void InitializeInterface();
 
   void DoStereoRender() override;
   bool InStereoRender;

vtkXLookingGlassRenderWindow.h
@@ -81,11 +81,90 @@ public:
    */
   std::string QuiltFileSuffix() const;
 
+  /**
+   * Turn on/off use of near and far clipping limits.
+   */
+  void SetUseClippingLimits(bool b);
+
+  /**
+   * Turn on/off use of near and far clipping limits.
+   */
+  bool GetUseClippingLimits() const;
+  vtkBooleanMacro(UseClippingLimits, bool);
+
+  /**
+   * Set/Get limit for the ratio of the far clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 1.2.
+   */
+  void SetFarClippingLimit(double d);
+
+  /**
+   * Set/Get limit for the ratio of the far clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 1.2.
+   */
+  double GetFarClippingLimit() const;
+
+  /**
+   * Set/Get limit for the ratio of the near clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 0.8.
+   */
+  void SetNearClippingLimit(double d);
+
+  /**
+   * Set/Get limit for the ratio of the near clipping plane to the focal
+   * distance. This is a mechanism to limit parallex and resulting
+   * ghosting when using the looking glass display. The typical value
+   * should be around 0.8.
+   */
+  double GetNearClippingLimit() const;
+
+  /**
+   * Check if a movie quilt is currently being recorded.
+   */
+  bool IsRecordingQuilt() const;
+
+  /**
+   * Set/Get which LookingGlass device to use. DeviceIndex starts at 0 and
+   * increases.
+   */
+  void SetDeviceIndex(int i);
+
+  /**
+   * Set/Get which LookingGlass device to use. DeviceIndex starts at 0 and
+   * increases.
+   */
+  int GetDeviceIndex() const;
+
+  /**
+   * Set/Get which LookingGlass device type to target. This allows a quilt to be
+   * generated for a device that is not connected in the future.
+   */
+  void SetDeviceType(const std::string &t);
+
+  /**
+   * Set/Get which LookingGlass device type to target. This allows a quilt to be
+   * generated for a device that is not connected in the future.
+   */
+  std::string GetDeviceType() const;
+
+  /**
+   * Returns a vector of available device types.
+   */
+  static std::vector<std::string> GetDeviceTypes();
+
 protected:
   vtkXLookingGlassRenderWindow();
   ~vtkXLookingGlassRenderWindow() override;
 
-  vtkLookingGlassInterface* Interface;
+  vtkLookingGlassInterface* Interface = nullptr;
+
+  void InitializeInterface();
 
   void DoStereoRender() override;
   bool InStereoRender;

vtkLookingGlassInterface.h
@@ -214,7 +214,7 @@ public:
   std::string QuiltFileSuffix() const;
 
   /**
-   * Check if the quilt is currently being recorded.
+   * Check if a movie quilt is currently being recorded.
    */
   bool IsRecordingQuilt() const { return this->IsRecording; }
 

vtkLookingGlassRenderWindowImpl.h
@@ -21,7 +21,17 @@ vtkStandardNewMacro(className);
 //------------------------------------------------------------------------------
 className::className()
 {
-  this->Interface = vtkLookingGlassInterface::New();
+  InitializeInterface();
+}
+
+//------------------------------------------------------------------------------
+void className::InitializeInterface()
+{
+  if (!this->Interface)
+  {
+    this->Interface = vtkLookingGlassInterface::New();
+  }
+
   this->Interface->Initialize();
   this->Interface->GetDisplaySize(this->Size[0], this->Size[1]);
   this->Interface->GetDisplayPosition(this->Position[0], this->Position[1]);
@@ -117,4 +127,92 @@ const char* className::MovieFileExtension()
 std::string className::QuiltFileSuffix() const
 {
   return this->Interface->QuiltFileSuffix();
+}
+
+//------------------------------------------------------------------------------
+void className::SetUseClippingLimits(bool b)
+{
+  this->Interface->SetUseClippingLimits(b);
+}
+
+//------------------------------------------------------------------------------
+bool className::GetUseClippingLimits() const
+{
+  return this->Interface->GetUseClippingLimits();
+}
+
+//------------------------------------------------------------------------------
+void className::SetFarClippingLimit(double d)
+{
+  this->Interface->SetFarClippingLimit(d);
+}
+
+//------------------------------------------------------------------------------
+double className::GetFarClippingLimit() const
+{
+  return this->Interface->GetFarClippingLimit();
+}
+
+//------------------------------------------------------------------------------
+void className::SetNearClippingLimit(double d)
+{
+  this->Interface->SetNearClippingLimit(d);
+}
+
+//------------------------------------------------------------------------------
+double className::GetNearClippingLimit() const
+{
+  return this->Interface->GetNearClippingLimit();
+}
+
+//------------------------------------------------------------------------------
+bool className::IsRecordingQuilt() const
+{
+  return this->Interface->IsRecordingQuilt();
+}
+
+//------------------------------------------------------------------------------
+void className::SetDeviceIndex(int i)
+{
+  this->Interface->SetDeviceIndex(i);
+}
+
+//------------------------------------------------------------------------------
+int className::GetDeviceIndex() const
+{
+  return this->Interface->GetDeviceIndex();
+}
+
+//------------------------------------------------------------------------------
+void className::SetDeviceType(const std::string& t)
+{
+  // Must re-build the interface from scratch
+  if (this->Interface)
+  {
+    this->Interface->ReleaseGraphicsResources(this);
+    this->Interface->Delete();
+    this->Interface = nullptr;
+  }
+
+  this->Interface = vtkLookingGlassInterface::New();
+  this->Interface->SetDeviceType(t);
+  InitializeInterface();
+}
+
+//------------------------------------------------------------------------------
+std::string className::GetDeviceType() const
+{
+  return this->Interface->GetDeviceType();
+}
+
+//------------------------------------------------------------------------------
+std::vector<std::string> className::GetDeviceTypes()
+{
+  std::vector<std::string> types;
+  for (const auto device : vtkLookingGlassInterface::GetDevices())
+  {
+    types.push_back(device.first);
+  }
+
+  return types;
 }

commit 23cd56032165848c60739898ccf613e35cbde62d

COMP: Streamline client project integration by explicitly including "vector"
This commit fixes regression introduced in b99fa7324 (Add several functions
to the render window) allowing client project to successfully build
without having to include "vector" header.

vtkCocoaLookingGlassRenderWindow.h
@@ -22,6 +22,7 @@
 
 #include "vtkCocoaRenderWindow.h"
 #include "vtkRenderingLookingGlassModule.h" // For export macro
+#include <vector>
 
 class vtkLookingGlassInterface;
 

vtkWin32LookingGlassRenderWindow.h
@@ -22,6 +22,7 @@
 
 #include "vtkRenderingLookingGlassModule.h" // For export macro
 #include "vtkWin32OpenGLRenderWindow.h"
+#include <vector>
 
 class vtkLookingGlassInterface;
 

vtkXLookingGlassRenderWindow.h
@@ -22,6 +22,7 @@
 
 #include "vtkRenderingLookingGlassModule.h" // For export macro
 #include "vtkXOpenGLRenderWindow.h"
+#include <vector>
 
 class vtkLookingGlassInterface;
 